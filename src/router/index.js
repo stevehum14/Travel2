cf8808edbede34bbb74ac7177f068f934ef12002
stevehum <stevehum@MacBook-Pro.local>
2021-10-20 15:58:41 +0800

    项目初始化-通用环境配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import fastClick from 'fastclick'
+import Home from '@/pages/home/Home'
 
 Vue.use(Router)
+fastClick.attach(document.body)
 
 export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }
   ]
 })
