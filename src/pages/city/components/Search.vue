66c4ff0622c54bc7d2f10508aa13d2160ac25d1a
hongxuechen <hongxuechen@hongxuechendeMac-mini.local>
2021-11-01 18:37:14 +0800

    city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="search">
+      <input type="text" class="search-input" placeholder="输入城市名称或者拼音">
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+<style lang="stylus" scoped>
+@import "~styles/varibles.styl"
+.search
+  height .72rem
+  padding 0 .1rem
+  background $bgColor
+  .search-input
+    box-sizing border-box
+    width 100%
+    height 0 .1rem
+    line-height .62rem
+    text-align center
+    border-radius .06rem
+    color #666
+</style>
