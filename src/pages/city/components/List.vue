44abab9d42d78ad190d2adfdf699b7ac1ec2f404
hongxuechen <hongxuechen@hongxuechendeMac-mini.local>
2021-11-02 15:19:51 +0800

    city

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -1,106 +1,113 @@
 <template>
-  <div class="list">
-    <div class="area">
-      <div class="title border-topbottom">当前城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">热门城市</div>
-      <div class="button-list">
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-        <div class="button-wrapper">
-          <div class="button">北京</div>
-        </div>
-      </div>
-    </div>
-    <div class="area">
-      <div class="title border-topbottom">A</div>
-      <div class="item-list">
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
         </div>
       </div>
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">
-          阿拉尔
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
         </div>
-        <div class="item border-bottom">
-          阿拉尔
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
         </div>
-        <div class="item border-bottom">
-          阿拉尔
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
         </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-      </div>
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-      </div>
-      <div class="title border-topbottom">B</div>
-      <div class="item-list">
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
-        </div>
-        <div class="item border-bottom">
-          阿拉尔
+        <div class="title border-topbottom">B</div>
+        <div class="item-list">
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
+          <div class="item border-bottom">
+            阿拉尔
+          </div>
         </div>
       </div>
     </div>
   </div>
 </template>
 <script>
+import Bscroll from 'better-scroll'
 export default {
-  name: 'CityList'
+  name: 'CityList',
+  mounted () {
+    this.scroll = new Bscroll(this.$refs.wrapper)
+  }
+
 }
 </script>
 <style lang="stylus" scoped>
@@ -123,27 +130,27 @@ export default {
   bottom 0
   //background red
   .title
-    line-height .44rem
+    line-height .54rem
     background #eee
     padding-left .2rem
     color #666
     font-size .26rem
-.button-list
-  padding .1rem .6rem .1rem .1rem
-  overflow hidden
-  .button-wrapper
-    float left
-    width 33.3%
-    .button
-      text-align center
-      margin .1rem
-      border .02rem solid #ccc
-      padding .1rem 0
-      border-radius .06rem
-.item-list
-  .item
-    line-height .76rem
-    color #666
-    padding-left .2rem
+  .button-list
+    padding .1rem .6rem .1rem .1rem
+    overflow hidden
+    .button-wrapper
+      float left
+      width 33.3%
+      .button
+        text-align center
+        margin .1rem
+        border .02rem solid #ccc
+        padding .1rem 0
+        border-radius .06rem
+  .item-list
+    .item
+      line-height .76rem
+      color #666
+      padding-left .2rem
 
 </style>
