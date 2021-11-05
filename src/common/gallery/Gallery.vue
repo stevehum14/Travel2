842ac30274e3d972afec633dacc8416e7424ae3a
hongxuechen <hongxuechen@hongxuechendeMac-mini.local>
2021-11-05 15:19:38 +0800

    banner-finish

---
+++ src/common/gallery/Gallery.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="container" @click="handleGalleryClick">
+    <div class="wrapper">
+      <swiper :option="swiperOption">
+        <!-- slides -->
+        <swiper-slide v-for="(item,index) of imgs" :key="index">
+          <img class="gallery-img" :src="item" alt="">
+        </swiper-slide>
+        <div class="swiper-pagination" slot="pagination"></div>
+      </swiper>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'CommonGallery',
+  props: {
+    imgs: {
+      type: Array,
+      default () {
+        return []
+      }
+    }
+  },
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        paginationType: 'fraction',
+        observeParents: true,
+        observer: true
+      }
+    }
+  },
+  methods: {
+    handleGalleryClick () {
+      this.$emit('galleryClose')
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.container >>> .swiper-container
+  overflow inherit
+.container
+  display flex
+  flex-direction column
+  justify-content center
+  z-index 99
+  position fixed
+  left 0
+  right 0
+  top 0
+  bottom 0
+  background #000
+  .wrapper
+    //overflow hidden
+    width 100%
+    height 0
+    padding-bottom 100%
+    background #fff
+    .gallery-img
+      width 100%
+    .swiper-pagination
+      color #fff
+      bottom -1rem
+</style>
